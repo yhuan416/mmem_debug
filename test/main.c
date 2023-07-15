795b170338edfd2abfb57e6068cc1cc4f9291648
yhuan416 <1290635392@qq.com>
2023-07-15 17:11:22 +0800

    feat: update CMAKE files

--- test/main.c
+++ test/main.c
@@ -1,29 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <CUnit/Basic.h>
 
-#include "mmem_debug.h"
+void test_add(void) {
+    CU_ASSERT_EQUAL(2 + 2, 4);
+}
 
-int main(int argc, char const *argv[])
-{
-    char dump_buf[1024] = {0};
+void test_sub(void) {
+    CU_ASSERT_EQUAL(2 - 2, 0);
+}
 
-    printf("Hello world!\n");
-
-    char *buf = (char *)malloc(1024);
-    if (buf == NULL) {
-        printf("malloc failed!\n");
-        return -1;
-    }
-
-    printf("malloc success!, %p\n", buf);
-
-    mmem_dump(argc, argv, 100, dump_buf, 1024);
-    printf("%s\n", dump_buf);
-
-    free(buf);
-
-    mmem_dump(argc, argv, 100, dump_buf, 1024);
-    printf("%s\n", dump_buf);
-
+int main() {
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("test_suite", NULL, NULL);
+    
+    CU_add_test(suite, "test_add", test_add);
+    CU_add_test(suite, "test_sub", test_sub);
+    
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    
+    CU_basic_run_tests();
+    
+    CU_cleanup_registry();
+    
     return 0;
 }
